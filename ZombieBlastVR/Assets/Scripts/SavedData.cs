321271303c1a9f27407394cb53bfdd9199a37b5a
mobina <mobina.km@gmail.com>
2023-02-27 21:57:05 +0330

    add load and save scripts

---
+++ ZombieBlastVR/Assets/Scripts/SavedData.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SavedData : MonoBehaviour
+{
+    public static SavedData control;
+    public string username;
+    public int score;
+    public bool[] levels;
+
+    private void Awake()
+    {
+        if(control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        } else if (control != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file;
+        if (File.Exists(Application.persistentDataPath+"/"+ username + ".dat"))
+        {
+            file = File.Open(Application.persistentDataPath + "/" + username + ".dat", FileMode.Open);
+
+        }
+        else
+        {
+            file = File.Create(Application.persistentDataPath + "/" + username + ".dat");
+
+        }
+        SaveDataHolder sdh = new SaveDataHolder(username, score, levels);
+        bf.Serialize(file, sdh);
+        file.Close();
+    }
+
+    public void LoadGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file;
+        if (File.Exists(Application.persistentDataPath + "/" + username + ".dat"))
+        {
+            file = File.Open(Application.persistentDataPath + "/" + username + ".dat", FileMode.Open);
+            SaveDataHolder sdh = (SaveDataHolder) bf.Deserialize(file);
+            username = sdh.GetUsername();
+            score = sdh.GetScore();
+            levels = sdh.GetLevels();
+            file.Close();
+        }
+        else
+        {
+            Debug.Log("file not found");
+        }
+
+    }
+}
+[Serializable]
+class SaveDataHolder
+{
+    string username;
+    int score;
+    bool[] levels;
+    public SaveDataHolder(string name, int scr, bool[] lvls)
+    {
+        username = name;
+        score = scr;
+        levels = new bool[lvls.Length];
+        for (int i = 0; i < lvls.Length; i++)
+        {
+            levels[i] = lvls[i];
+        }
+    }
+    public string GetUsername()
+    {
+        return username;
+    }
+    public int GetScore()
+    {
+        return score;
+    }
+    public bool[] GetLevels()
+    {
+        return levels; 
+    }
+}
+
